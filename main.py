86acbfb7a4f3be323937461716520bbd481387c5
Filipe Neves <filipe.nevser@gmail.com>
2024-06-20 23:45:58 -0300

    fix: update main function to iterate in input directory

--- main.py
+++ main.py
@@ -1,81 +1,84 @@
 import os
 import sys
-from time import time
 
 import SimpleITK as sitk
-import itk
 
 from src.config import ConfigManager
 from src.skull_stripping import skull_stripping
-# from src.denoise import denoise_image
+from src.denoise import denoise_image
 from src.image_io import load_nib_image, save_nib_image, save_sitk_image, load_sitk_image, save_nib_image_to_tempfile, save_sitk_image_to_tempfile, load_itk_image, save_itk_image, save_itk_image_to_tempfile, cleanup_tempfile
 from src.bias_field import remove_bias_field
-from src.utils import log, bcolors, normalize_dir_path
+from src.utils import log, bcolors, normalize_dir_path, list_files_in_directory
 from src.orientation import sync_images_orientation
 
-def main(mri_path, output_dir, config_path):
-  # Config
-  log("> Loading Config")
-  config_manager = ConfigManager(config_path)
-  # config = read_config(config_path)
-  log("> Config Loaded", bcolors.OKGREEN, True)
+def proccess_image(image_file_name, output_dir, config_manager):
+  log(f"\n# {image_file_name}", bcolors.OKCYAN)
 
-  save_stepwise = config_manager.get_config_value('GENERAL', 'SAVE_INTERMEDIATE_STEPS', default=False)
-  print(save_stepwise)
-  return
+  file_name_with_extension = os.path.basename(image_file_name)
+  file_name, file_extension = os.path.splitext(file_name_with_extension)
 
+  save_stepwise = config_manager.get_config_value('GENERAL', 'SAVE_INTERMEDIATE_STEPS', default=True)
 
   # Loading
-  log("> Loading Images")
-  original_work_image = load_itk_image(mri_path)
-  log("> Images Loaded", bcolors.OKGREEN, True)
-
-  if save_stepwise:
-    save_itk_image(original_work_image, f'./{output_dir}/1.ORIGINAL_ORIENTED.nii')
+  log("> Loading Image")
+  work_image = load_itk_image(image_file_name)
+  log("> Image Loaded", bcolors.OKGREEN, True)
 
   # Skull Stripping (NIB)
-  temp_path_original = save_itk_image_to_tempfile(original_work_image)
-  original_work_image = load_nib_image(temp_path_original)
+  temp_path_original = save_itk_image_to_tempfile(work_image)
+  work_image = load_nib_image(temp_path_original)
 
-  log("> Running Original Image Skull Stripping")
-  original_work_image, original_mask, time = skull_stripping(original_work_image)
-  log(f"> Original Image Skull Stripping finished (took {time:.2f}s)", bcolors.OKGREEN, True)
+  log("> Running Image Skull Stripping")
+  work_image, mask_image, time = skull_stripping(work_image)
+  log(f"> Image Skull Stripping finished (took {time:.2f}s)", bcolors.OKGREEN, True)
 
   if save_stepwise:
-    save_nib_image(original_work_image, f'./{output_dir}/2.ORIGINAL_STRIPPED.nii')
-    save_nib_image(original_mask, f'./{output_dir}/2.ORIGINAL_MASK.nii')
-
-  cleanup_tempfile(temp_path_original)
+    save_nib_image(work_image, f'{output_dir}/{file_name}-1_stripped{file_extension}')
+    save_nib_image(mask_image, f'{output_dir}/{file_name}-1_mask{file_extension}')
 
   # Denoising (NIB)
-  original_denoising_enabled = config_manager.get_config_value('DENOISE', 'ENABLED', default=True)
-  if original_denoising_enabled:
-    log("> Running Original Image Denoising")
-    original_work_image, time = denoise_image(original_work_image, original_mask)
-    log(f"> Original Image Denoising finished (took {time:.2f}s)", bcolors.OKGREEN, True)
+  denoising_enabled = config_manager.get_config_value('DENOISE', 'ENABLED', default=True)
+  if denoising_enabled:
+    log("> Running Image Denoising")
+    work_image, time = denoise_image(work_image, mask_image, config_manager)
+    log(f"> Image Denoising finished (took {time:.2f}s)", bcolors.OKGREEN, True)
 
-  if save_stepwise:
-    save_nib_image(original_work_image, f'./{output_dir}/3.ORIGINAL_DENOISED.nii')
+    if save_stepwise:
+      save_nib_image(work_image, f'{output_dir}/{file_name}-2_denoised{file_extension}')
 
   # Bias field removal (SITK)
   bias_field_removal_enabled = config_manager.get_config_value('BIAS_FIELD_REMOVAL', 'ENABLED', default=True)
   if bias_field_removal_enabled:
     log("> Running Bias Field Removal")
-    temp_path_original = save_nib_image_to_tempfile(original_work_image)
-    temp_path_original_mask = save_nib_image_to_tempfile(original_mask)
+    temp_path_work_image = save_nib_image_to_tempfile(work_image)
+    temp_path_mask_image = save_nib_image_to_tempfile(mask_image)
 
-    original_work_image = load_sitk_image(temp_path_original, sitk.sitkFloat32)
-    original_mask = load_sitk_image(temp_path_original_mask, sitk.sitkUInt8)
+    work_image = load_sitk_image(temp_path_work_image, sitk.sitkFloat32)
+    mask_image = load_sitk_image(temp_path_mask_image, sitk.sitkUInt8)
 
-    original_work_image, time = remove_bias_field(original_work_image, original_mask)
+    work_image, time = remove_bias_field(work_image, mask_image, config_manager)
 
     log(f"> Bias Field Removed (took {time:.2f}s)", bcolors.OKGREEN, True)
 
     if save_stepwise:
-      save_sitk_image(original_work_image, f'./{output_dir}/4.ORIGINAL_BIASFIELDED.nii')
+      save_sitk_image(work_image, f'{output_dir}/{file_name}-3_biasfielded{file_extension}')
 
-    cleanup_tempfile(temp_path_original)
-    cleanup_tempfile(temp_path_original_mask)
+    cleanup_tempfile(temp_path_work_image)
+    cleanup_tempfile(temp_path_mask_image)
+
+def main(input_dir, output_dir, config_path):
+  # Config
+  log("> Loading Config")
+  config_manager = ConfigManager(config_path)
+  log("> Config Loaded", bcolors.OKGREEN, True)
+
+  # Input Files
+  log("> Reading Input Directory")
+  input_images = list_files_in_directory(input_dir)
+  log("> Input Directory Readed", bcolors.OKGREEN, True)
+
+  for path in input_images:
+    proccess_image(path, output_dir, config_manager)
 
 if __name__ == "__main__":
   script_dir = os.path.dirname(__file__)
@@ -93,10 +96,10 @@ if __name__ == "__main__":
     sys.exit(1)
 
   if not os.path.exists(config_path):
-    print(f"No configuration file found at {config_path}. Please run 'python main.py --generate-config' to create a default config file.")
+    print(f"No configuration file found at {config_path}.\nPlease run 'python3 main.py --generate-config' to create a default config file.")
     sys.exit(1)
 
-  input_dir = normalize_dir_path(sys.argv[1])
-  output_dir = normalize_dir_path(sys.argv[2])
-  
+  input_dir = os.path.abspath(normalize_dir_path(sys.argv[1]))
+  output_dir = os.path.abspath(normalize_dir_path(sys.argv[2]))
+
   main(input_dir, output_dir, config_path)
